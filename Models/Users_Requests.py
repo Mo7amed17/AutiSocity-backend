ebc9ba9d2e8f887127bd8603fa559f8ddfe72d5b
Ahmed Gehad Sonbol <ahmedsonbl9@gmail.com>
2024-03-25 22:48:27 +0200

    test api remotely

--- Models/Users_Requests.py
+++ Models/Users_Requests.py
@@ -11,14 +11,14 @@ usersblp = Blueprint("usersblp",__name__,static_folder="static",template_folder=
 # ================== LOGIN [POST] =========================
 
 # @cross_origin(origin='*',headers=['Content-Type','x-access-token'])
-@usersblp.route("/login",methods=['GET'])
+@usersblp.route("/login",methods=['POST'])
 def login():
      
-     # request_data = request.get_json()
-     # if request_data is None or "email" not in  request_data or "password" not in request_data:
-     #      return jsonify({'Message':'email & password are requeired !'}) ,400
-     # return UsersDB.login(request_data)
-     return ({'message':'koko'}),200
+     request_data = request.get_json()
+     if request_data is None or "email" not in  request_data or "password" not in request_data:
+          return jsonify({'Message':'email & password are requeired !'}) ,400
+     return UsersDB.login(request_data)
+     
 
 
 
