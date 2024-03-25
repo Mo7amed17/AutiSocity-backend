06390983a06002412bcd3406ac14e16d454459e6
Mo7amed17 <mo7amedatef17@gmail.com>
2024-03-25 05:54:35 +0200

    edit db connection

--- DB_Connections/DB_connections.py
+++ DB_Connections/DB_connections.py
@@ -1,21 +1,27 @@
-
 import pyodbc
 
+server = 'sql5088.site4now.net'
+database = 'db_aa6e06_autismdb'
+username = 'db_aa6e06_autismdb_admin'
+password = 'Mo7amedatef17'
 
+# بناء سلسلة الاتصال
+connection_string = 'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
 
 class LinkDatabase:
-    {}
-    
-    # def __init__(self):
-    #     self.conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};SERVER=.;DATABASE=AutismDB;Trusted_Connection=yes')
+    # Try-Catch بنستعملها دايما عشان لو في ايرور الكود يطلع الايرور ويكمل
+    # try هيجرب الكود ولو ظبط يكمل ماظبطش يطلع ايرور 
+    # catch هنا بيستقبل الايرور ويقولك في ايرور ويروح يكمل باقي ويقفل نفسه ويروح يكمل باقي الكود عادي بدون مايقف 
+    def __init__(self):
+        try:
+            self.conn = pyodbc.connect(connection_string)            
 
-    #     self.conn.setdecoding(pyodbc.SQL_CHAR , encoding='utf8')
-    #     self.conn.setdecoding(pyodbc.SQL_WCHAR , encoding='utf8')
-    #     self.conn.setencoding(encoding='utf8')
-    #     # self.conn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-32le')
+            self.conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
+            self.conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+            self.conn.setencoding(encoding='utf-8')
 
-    #     self.cursor = self.conn.cursor()
-    
-
-
-
+            self.cursor = self.conn.cursor()
+            print("Conected")
+            
+        except Exception as e:
+            print("Data Base Error", e)
