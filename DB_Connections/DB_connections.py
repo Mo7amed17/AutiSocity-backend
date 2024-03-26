adc71d07c704a57554a54497ccc2d7cac316bde2
Mo7amed17 <mo7amedatef17@gmail.com>
2024-03-26 15:10:56 +0200

    test database

--- DB_Connections/DB_connections.py
+++ DB_Connections/DB_connections.py
@@ -1,4 +1,4 @@
-import pyodbc
+import pymssql
 
 server = 'sql5088.site4now.net'
 database = 'db_aa6e06_autismdb'
@@ -6,23 +6,21 @@ username = 'db_aa6e06_autismdb_admin'
 password = 'Mo7amedatef17'
 
 # بناء سلسلة الاتصال
-connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
-print(pyodbc.drivers())
+connection_string = {
+    'server': server,
+    'user': username,
+    'password': password,
+    'database': database,
+    'autocommit': True  # يمكنك تعيين هذا على True إذا كنت ترغب في تمكين التعامل مع البيانات بشكل تلقائي
+}
+
 class LinkDatabase:
     
-    # Try-Catch بنستعملها دايما عشان لو في ايرور الكود يطلع الايرور ويكمل
-    # try هيجرب الكود ولو ظبط يكمل ماظبطش يطلع ايرور 
-    # catch هنا بيستقبل الايرور ويقولك في ايرور ويروح يكمل باقي ويقفل نفسه ويروح يكمل باقي الكود عادي بدون مايقف 
     def __init__(self):
         try:
-            self.conn = pyodbc.connect(connection_string)            
-
-            self.conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
-            self.conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
-            self.conn.setencoding(encoding='utf-8')
-
+            self.conn = pymssql.connect(**connection_string)
             self.cursor = self.conn.cursor()
-            print("Conected")
+            print("Connected")
             
         except Exception as e:
-            print("Data Base Error", e)
+            print("Database Error:", e)
