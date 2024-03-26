bd9cf5273616c1cfb83b176166b45d4bef035a6c
Mo7amed17 <mo7amedatef17@gmail.com>
2024-03-26 04:17:47 +0200

    test database

--- DB_Connections/DB_connections.py
+++ DB_Connections/DB_connections.py
@@ -1,13 +1,12 @@
-import os
 import pyodbc
 
 server = 'sql5088.site4now.net'
 database = 'db_aa6e06_autismdb'
 username = 'db_aa6e06_autismdb_admin'
 password = 'Mo7amedatef17'
-os.environ["ODBCSYSINI"] = "/home/mysite/AutiSocity-backend"
+
 # بناء سلسلة الاتصال
-# connection_string = 'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
+connection_string = 'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
 
 class LinkDatabase:
     # Try-Catch بنستعملها دايما عشان لو في ايرور الكود يطلع الايرور ويكمل
@@ -15,8 +14,8 @@ class LinkDatabase:
     # catch هنا بيستقبل الايرور ويقولك في ايرور ويروح يكمل باقي ويقفل نفسه ويروح يكمل باقي الكود عادي بدون مايقف 
     def __init__(self):
         try:
-            # self.conn = pyodbc.connect(connection_string)            
-            self.conn = pyodbc.connect('DSN=SQLServer;Uid=db_aa6e06_autismdb_admin;Pwd=Mo7amedatef17;Encrypt=yes;Connection Timeout=30;')
+            self.conn = pyodbc.connect(connection_string)            
+
             self.conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
             self.conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
             self.conn.setencoding(encoding='utf-8')
