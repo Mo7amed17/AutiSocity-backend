85c8734603266e3ffe6a5966080e2a5b1178688b
Mo7amed17 <mo7amedatef17@gmail.com>
2024-03-26 03:57:26 +0200

    test database

--- DB_Connections/DB_connections.py
+++ DB_Connections/DB_connections.py
@@ -1,33 +1,22 @@
-import pyodbc
 import os
-import platform
-
+import pyodbc
 
 server = 'sql5088.site4now.net'
 database = 'db_aa6e06_autismdb'
 username = 'db_aa6e06_autismdb_admin'
 password = 'Mo7amedatef17'
-
-
-os.environ["ODBCSYSINI"] = "/home/AutiSociety"
-
+os.environ["ODBCSYSINI"] = "/home/mysite/AutiSocity-backend"
 # بناء سلسلة الاتصال
-# connection_string = 'DRIVER={SQL SERVER};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password+';Connection Timeout=30'
+# connection_string = 'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
 
 class LinkDatabase:
     # Try-Catch بنستعملها دايما عشان لو في ايرور الكود يطلع الايرور ويكمل
     # try هيجرب الكود ولو ظبط يكمل ماظبطش يطلع ايرور 
     # catch هنا بيستقبل الايرور ويقولك في ايرور ويروح يكمل باقي ويقفل نفسه ويروح يكمل باقي الكود عادي بدون مايقف 
     def __init__(self):
-        
-        # print(pyodbc.drivers())
-        # print(os.name)
-        # print(platform.system())
         try:
-            # self.conn = pyodbc.connect(connection_string)   
-            self.conn = pyodbc.connect('DSN=sqlserverdatasource;Uid=db_aa6e06_autismdb_admin;Pwd=Mo7amedatef17;Encrypt=yes;Connection Timeout=30;')
-         
-
+            # self.conn = pyodbc.connect(connection_string)            
+            conn = pyodbc.connect('DSN=sqlserverdatasource;Uid=db_aa6e06_autismdb_admin;Pwd=Mo7amedatef17;Encrypt=yes;Connection Timeout=30;')
             self.conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
             self.conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
             self.conn.setencoding(encoding='utf-8')
