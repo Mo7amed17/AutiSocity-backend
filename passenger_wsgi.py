b2b6b96523c0c36144c25d4738492cd819266c23
Mo7amed17 <mo7amedatef17@gmail.com>
2024-03-26 23:28:11 +0200

    test deploy

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-from importlib.machinery import SourceFileLoader
-import sys, os
-sys.path.append(os.getcwd())
-
-wsgi = SourceFileLoader('wsgi', 'application.py').load_module()
-
-application = wsgi.app
