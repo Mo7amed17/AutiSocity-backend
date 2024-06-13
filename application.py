d024c6742af1741c5bae69c93868f76928bce43d
Ahmed Gehad Sonbol <ahmedsonbl9@gmail.com>
2024-06-13 13:35:45 +0300

    add socket data

--- application.py
+++ application.py
@@ -7,7 +7,9 @@ from Models.Posts_Requests import postsblp
 
 from flask_cors import CORS ,cross_origin
 
-from flask_socketio import SocketIO ,send
+from socket_Implementation.events import socketio
+
+# from flask_socketio import SocketIO ,send
 
 
 
@@ -27,14 +29,16 @@ app.register_blueprint(postsblp , url_prefix="/api/posts")
 app.config['JSON_AS_ASCII'] = False
 app.config['SECRET_KEY'] = '654321'
 
-socketio = SocketIO(app,cors_allowed_origins = '*')
+# socketio = SocketIO(app,cors_allowed_origins = '*')
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message :' + message)
-    if message != 'User connected!':
-        send(message,broadcast = True)
+# @socketio.on('message')
+# def handle_message(message):
+#     print('received message :' + message)
+#     if message != 'User connected!':
+#         send(message,broadcast = True)
 
+
+socketio.init_app(app=app)
 
 if __name__ == '__main__':
     # app.run(debug=False,host='0.0.0.0')
