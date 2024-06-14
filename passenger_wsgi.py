dce6ecc4d3d8624d1aa75f28a098cce80b8d6653
Mo7amed17 <mo7amedatef17@gmail.com>
2024-06-14 20:01:24 +0300

    test db

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,14 @@
-import sys, os
+import sys
+import os
 
-base_path = "/home/origmtie/virtualenv/autisociety-api.original-business.com/3.9/lib/python3.9/site-packages"
+# تحديد المسار للجذر الرئيسي للتطبيق
+app_root = '/home/origmtie/autisociety-api.original-business.com'
+sys.path.insert(0, app_root)
 
-sys.path.append(base_path)
+# تفعيل البيئة الافتراضية
+venv_path = '/home/origmtie/virtualenv/autisociety-api.original-business.com/3.9'
+activate_env = os.path.join(venv_path, 'bin/activate_this.py')
+exec(open(activate_env).read(), {'__file__': activate_env})
 
+# استيراد التطبيق
 from application import app as application
