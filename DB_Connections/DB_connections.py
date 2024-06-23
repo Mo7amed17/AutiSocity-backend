3bbe183a008c17d1739c1f809bb185b3f4cfedbb
Ahmed Gehad Sonbol <ahmedsonbl9@gmail.com>
2024-06-24 02:15:03 +0300

    5oood

--- DB_Connections/DB_connections.py
+++ DB_Connections/DB_connections.py
@@ -2,20 +2,41 @@ import pymssql
 # import pyodbc
 
 
-server = 'sql6028.site4now.net'
-database = 'db_aa9b62_autisocietydb'
-username = 'db_aa9b62_autisocietydb_admin'
-password = 'Sonbolmyasp123'
+# server = 'sql6028.site4now.net'
+# database = 'db_aa9b62_autisocietydb'
+# username = 'db_aa9b62_autisocietydb_admin'
+# password = 'Sonbolmyasp123'
+
+# server = 'DESKTOP-T7CCNDN\MSSQLSERVER01'
+# database = 'AutismDBNew'
+# username = 'auti'
+# password = '123456'
+
+server = '.'
+database = 'AutismDB'
+username = 'autism'
+password = '123456'
+
+#AutismDBNew
 
 # بناء سلسلة الاتصال
 connection_string = {
-    'server': server,
-    'user': username,
-    'password': password,
-    'database': database,
-    'port' : 1433 ,
-    'autocommit': True  # يمكنك تعيين هذا على True إذا كنت ترغب في تمكين التعامل مع البيانات بشكل تلقائي
+    # 'Integrated_Security':True,
+    'server': 'DESKTOP-T7CCNDN\MSSQLSERVER01',
+    'user': 'auti',
+    'password': '123456',
+    'database': 'AutismDBNew',
+    
+    # 'port' : 1433 ,
+    # 'autocommit': True,  # يمكنك تعيين هذا على True إذا كنت ترغب في تمكين التعامل مع البيانات بشكل تلقائي
+    
 }
+print(connection_string)
+
+connection_string['server'] = connection_string['server'].replace('\\\\','\\')
+print(connection_string)
+# conn = pymssql.connect(server = 'DESKTOP-T7CCNDN\MSSQLSERVER01', database = 'AutismDBNew')
+
 class LinkDatabase:
     
     def __init__(self):
@@ -26,7 +47,8 @@ class LinkDatabase:
     def connect(self):
         try:
             # self.conn = pyodbc.connect(connection_string)
-            self.conn = pymssql.connect(**connection_string)
+            self.conn = pymssql.connect(server='DESKTOP-T7CCNDN\MSSQLSERVER01',database='AutismDBNew',user='auti',password='123456')
+            # self.conn = pymssql.connect(server='.\\SQLEXPRESS2',database='BookingSystemDB',user='sa',password='123')
             self.cursor = self.conn.cursor()
             print("Connected")
         except Exception as e:
